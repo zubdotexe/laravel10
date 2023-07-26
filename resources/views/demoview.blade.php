2ec5283b71aa63a40c8e7349ba1f1849bd11f4fd
zubdotexe <ahmadzubairbu@gmail.com>
2023-07-26 23:31:28 +0600

    second commit

---
+++ resources/views/demoview.blade.php
@@ -0,0 +1,11 @@
+<h1>
+    echo "hahahaha is this pain or is this relief";
+</h1>
+
+<h2>
+    {{ $username }}
+</h2>
+
+<h2>
+    {{ $id }}
+</h2>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/demo', function() {
+    echo "hello world";
+});
+
+Route::post('/test', function() {
+    echo "testing";
+});
+
+Route::any('/test', function() {    # post or get jekono theke chole ashbe
+    echo "testing";
+});
+
+// Route::get('/demoview', function() {
+//     return view('demoview');
+// }); 
+
+Route::get('demoview/{username}/{id?}', function($username, $id=null) {
+    // echo $username." ";
+    // echo $id;
+
+    $data = compact('username', 'id');
+    // print_r($data);
+    return view('demoview')->with($data);
+});
